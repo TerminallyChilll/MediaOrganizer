9651e8db0cb94ba9c5803148000ed75480d79677
Tom M <73926467+TerminallyChilll@users.noreply.github.com>
2026-04-01 19:52:36 -0400

    Upload media organizer

---
+++ undo_organize.bat
@@ -0,0 +1,11 @@
+@echo off
+chcp 65001
+echo Undoing organize changes...
+mkdir "\\Homeward1\Media Storage\TV Shows\Secret Lives Of Mormon Wives\Season 2\The.Secret.Lives.of.Mormon.Wives.S02E11.720p.HEVC.x265-MeGusta"
+move "\\Homeward1\Media Storage\TV Shows\Secret Lives Of Mormon Wives\Season 2\The.Secret.Lives.of.Mormon.Wives.S02E11.720p.HEVC.x265-MeGusta.nfo" "\\Homeward1\Media Storage\TV Shows\Secret Lives Of Mormon Wives\Season 2\The.Secret.Lives.of.Mormon.Wives.S02E11.720p.HEVC.x265-MeGusta\The.Secret.Lives.of.Mormon.Wives.S02E11.720p.HEVC.x265-MeGusta.nfo"
+move "\\Homeward1\Media Storage\TV Shows\Secret Lives Of Mormon Wives\Season 2\The.Secret.Lives.of.Mormon.Wives.S02E11.720p.HEVC.x265-MeGusta.mkv" "\\Homeward1\Media Storage\TV Shows\Secret Lives Of Mormon Wives\Season 2\The.Secret.Lives.of.Mormon.Wives.S02E11.720p.HEVC.x265-MeGusta\The.Secret.Lives.of.Mormon.Wives.S02E11.720p.HEVC.x265-MeGusta.mkv"
+mkdir "\\Homeward1\Media Storage\TV Shows\Secret Lives Of Mormon Wives\Season 2\The Secret Lives Of Mormon Wives S02E11 720p WEB H264-JFF"
+move "\\Homeward1\Media Storage\TV Shows\Secret Lives Of Mormon Wives\Season 2\The Secret Lives Of Mormon Wives S02E11 720p WEB H264-JFF.nfo" "\\Homeward1\Media Storage\TV Shows\Secret Lives Of Mormon Wives\Season 2\The Secret Lives Of Mormon Wives S02E11 720p WEB H264-JFF\The Secret Lives Of Mormon Wives S02E11 720p WEB H264-JFF.nfo"
+move "\\Homeward1\Media Storage\TV Shows\Secret Lives Of Mormon Wives\Season 2\The Secret Lives Of Mormon Wives S02E11 720p WEB H264-JFF.mkv" "\\Homeward1\Media Storage\TV Shows\Secret Lives Of Mormon Wives\Season 2\The Secret Lives Of Mormon Wives S02E11 720p WEB H264-JFF\The Secret Lives Of Mormon Wives S02E11 720p WEB H264-JFF.mkv"
+echo Done!
+pause
